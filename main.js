04be602dbbf5b73b2dad5a51eb2f49265b14b56d
roolruri <dldudgus543@gmail.com>
2020-06-26 16:58:42 +0900

    JS: remove active from Nav

--- main.js
+++ main.js
@@ -59,18 +59,24 @@ workBtnContainer.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
-  projectContainer.classList.add("anim-out");
 
+  //Remove selection from the previous item and select the new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+
+  target.classList.add("selected");
+
+  projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
-      //console.log(project.dataset.type);
       if (filter === "*" || filter === project.dataset.type) {
         project.classList.remove("invisible");
       } else {
         project.classList.add("invisible");
       }
     });
-
     projectContainer.classList.remove("anim-out");
   }, 300);
 });
